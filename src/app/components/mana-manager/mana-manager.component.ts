24523301971b9dbbf4ba968a6f5ede2519be6a5b
Stanley Situ <ssitu@dayspring-tech.com>
2017-03-20 15:30:44 -0700

    interact with mana count

--- src/app/components/mana-manager/mana-manager.component.ts
+++ src/app/components/mana-manager/mana-manager.component.ts
@@ -3,10 +3,10 @@ import { Component } from "@angular/core";
 @Component ({
     selector: "mana-manager",
     template: require("./mana-manager.component.html"),
-    styles: [require("./mana-manager.component.scss")]
+    // styles: [require("./mana-manager.component.scss")]
 })
 
 export class ManaManagerComponent {
     constructor() {
     }
-}
+} 
--- src/app/components/mana/mana.component.ts
+++ src/app/components/mana/mana.component.ts
@@ -3,15 +3,17 @@ import { Mana } from "../model/mana";
 
 @Component({
     selector: "mana",
-    template: require("./mana.component.html")
+    template: require("./mana.component.html"),
+    styles: [require("./mana.component.scss")]
 })
 
 export class ManaComponent {
     @Input() colorType: string = undefined;
 
     private mana: Mana = null;
-    private manaLeft: number = 0;
-    private manaUsed: number = 0;
+    private manaPool: number = 0;
+
+    public manaLeft : number = 0;
 
     constructor() { }
 	
@@ -19,37 +21,62 @@ export class ManaComponent {
         this.mana = new Mana(this.colorType);
 	}
 
+    getManaSymbol() : string {
+        let manaImg:string = undefined;
+
+        switch(this.mana.getColor()) {
+            case "RED":
+                manaImg = "Mana_R.png";
+                break;
+            case "BLUE":
+                manaImg = "Mana_U.png";
+                break;
+            case "GREEN":
+                manaImg = "Mana_G.png";
+                break;
+            case "WHITE":
+                manaImg = "Mana_W.png";
+                break;
+            case "BLACK":
+                manaImg = "Mana_B.png";
+                break;
+            case "MULTICOLOR":
+                manaImg = "Mana_Rainbow.png";
+                break;
+            default:
+                manaImg = "Mana_Colorless.png";
+                break;
+        }
+
+        return "/public/images/mana-color/" + manaImg;
+    }
+
     getManaTotal() : number {
         return this.mana.getManaTotal();
     }
 
-    getManaSymbol() : string {
-        let png:string = undefined;
+    addMana() : void {
+        this.mana.addManaTotal();
+        this.manaPool ++;
+        this.manaLeft ++;
+    }
 
-        switch(this.mana.getColor()) {
-            case "RED":
-                png = "Mana_R.png";
-                break;
-            case "BLUE":
-                png = "Mana_U.png";
-                break;
-            case "GREEN":
-                png = "Mana_G.png";
-                break;
-            case "WHITE":
-                png = "Mana_W.png";
-                break;
-            case "BLACK":
-                png = "Mana_B.png";
-                break;
-            case "MULTICOLOR":
-                png = "Mana_Rainbow.png";
-                break;
-            default:
-                png = "Mana_Colorless.png";
-                break;
+    spendMana() : void {
+        if (this.manaLeft > 0) {
+            this.manaLeft --;
         }
+    }
 
-        return "/public/images/mana-color/" + png;
+    endPhase() : void {
+        this.manaPool = this.manaLeft;
+    }
+
+    endTurn() : void {
+        this.manaPool = this.getManaTotal();
+        this.manaLeft = this.getManaTotal();
+    }
+
+    undo() : void {
+        this.manaLeft = this.manaPool;
     }
 }
